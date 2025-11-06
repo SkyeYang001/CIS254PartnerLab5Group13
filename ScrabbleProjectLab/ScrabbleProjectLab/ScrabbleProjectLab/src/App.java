dd174cf69fce2d667e0b0a091a41a74034dc0dca
Skye's Fighting Eggs <yang.skye06@gmail.com>
2025-11-06 11:22:31 -0800

    Creation Commit
    Using the class created files.

---
+++ ScrabbleProjectLab/ScrabbleProjectLab/ScrabbleProjectLab/src/App.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Scrabble!");
+        ArrayList<Tile> tiles = new ArrayList<Tile>();
+        createTiles(tiles);
+        Random rand = new Random();
+        int randomTile = rand.nextInt(26);
+        System.out.println(randomTile);
+        System.out.println(tiles.get(randomTile));
+    }
+
+    public static void createTiles(ArrayList<Tile> tiles) {
+        tiles.add(new Tile('A', 1));
+        tiles.add(new Tile('B', 3));
+        tiles.add(new Tile('C', 3));
+        tiles.add(new Tile('D', 2));
+        tiles.add(new Tile('E', 1));
+        tiles.add(new Tile('F', 4));
+        tiles.add(new Tile('G', 2));
+        tiles.add(new Tile('H', 4));
+        tiles.add(new Tile('I', 1));
+        tiles.add(new Tile('J', 8));
+        tiles.add(new Tile('K', 5));
+        tiles.add(new Tile('L', 1));
+        tiles.add(new Tile('M', 3));
+        tiles.add(new Tile('N', 1));
+        tiles.add(new Tile('O', 1));
+        tiles.add(new Tile('P', 3));
+        tiles.add(new Tile('Q', 10));
+        tiles.add(new Tile('R', 1));
+        tiles.add(new Tile('S', 1));
+        tiles.add(new Tile('T', 1));
+        tiles.add(new Tile('U', 1));
+        tiles.add(new Tile('V', 4));
+        tiles.add(new Tile('W', 4));
+        tiles.add(new Tile('X', 8));
+        tiles.add(new Tile('Y', 4));
+        tiles.add(new Tile('Z', 10));
+    }
+}
---
+++ ScrabbleProjectLab/ScrabbleProjectLab/ScrabbleProjectLab/src/Tile.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+/**
+ * Homework 13
+ * Description:
+ * Tile class, creates a tile assigned to the called letter with their
+ * containing their direct value scores.
+ * 
+ * @author Skye Yang
+ * @since 11/06/2025
+ */
+
+public class Tile {
+    private char letter;
+    private int value;
+
+    /**
+     * The default constructor method
+     * 
+     * @param letter
+     * @param value
+     */
+    public Tile(char letter, int value) {
+        this.letter = letter;
+        this.value = value;
+    }
+
+    /**
+     * Sets tile letter
+     * 
+     * @param letter
+     */
+    public void setLetter(char letter) {
+        this.letter = letter;
+    }
+
+    /**
+     * Sets tile value
+     * 
+     * @param value
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    /**
+     * Gets tile letter
+     * 
+     * @return tile's letter
+     */
+    public char getLetter() {
+        return this.letter;
+    }
+
+    /**
+     * Gets value
+     * 
+     * @return tile's value
+     */
+    public int getValue() {
+        return this.value;
+    }
+
+    /**
+     * if tile equals to another tile
+     * 
+     * @param other
+     * @return true, if both tiles are equal
+     */
+    public boolean equals(Tile other) {
+        return this.letter == other.letter;
+    }
+
+    /**
+     * Creates a string, telling the tile's letter and value
+     * 
+     * @return printed string
+     */
+    public String toString() {
+        return String.format("%c - %d", letter, value);
+    }
+
+    /**
+     * Increasing score by the tile's value
+     * 
+     * @param score
+     * @return score added with tile's value
+     */
+    public int addScore(int score) {
+        return score + value;
+    }
+
+    /**
+     * Adds the tile's letter to word
+     * @param word
+     * @return word added with tile's letter
+     */
+    public String addWord(String word) {
+        return word + letter;
+    }
+}
